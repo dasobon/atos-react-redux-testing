5d06b43dbdd0d5623f7b47398a06a8d668de542b
dasobon <daria.sobon@atos.net>
2019-09-03 11:07:41 +0200

    new testcase

--- src/App.test.js
+++ src/App.test.js
@@ -3,8 +3,15 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 //unit test, co jest prawdziwe, a co mockowane
-it('renders without crashing', () => {
+it('should render without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+it('should render without crashing(with prop all=false)', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App all={false}/>, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
